0fcf7cae5327cc8ecde2ffa03bb537680fb3bd09
jmxxii <jeysonmmurillo@gmail.com>
2021-09-11 14:31:09 -0400

    Configure header

--- src/components/navigation/NavBar.vue
+++ src/components/navigation/NavBar.vue
@@ -1,13 +1,53 @@
 <template>
-  <h1>Navigation Bar</h1>
+  <header id="nav">
+    <div class="nav-container">
+      <div class="logo-container">
+        <img
+          class="repzio-logo"
+          :src="logoUri"
+          alt="repzio-logo"
+        />
+      </div>
+
+      <div class="nav-links">
+        <p>nav links</p>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
+import { logo } from '@/mock_env';
+
 export default {
   name: 'NavBar',
+  computed: {
+    logoUri() {
+      const paramConfig = '?width=100';
+
+      return logo + paramConfig;
+    },
+  },
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+
+#nav {
+  background-color: #42b983;
+  width: 100%;
+
+  .nav-container {
+    display: flex;
+    justify-content: space-between;
+
+    .logo-container {
+      width: 120px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  }
+}
 
 </style>
---
+++ src/mock_env.js
@@ -0,0 +1,3 @@
+module.exports = {
+  logo: 'https://images.repzio.com/productimages/772/logo772_lg.jpg',
+};
