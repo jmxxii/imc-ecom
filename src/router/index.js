03841698eebbd19e2d220ab0059840f3446ad1ba
jmxxii <jeysonmmurillo@gmail.com>
2021-09-11 12:16:17 -0400

    Configure routes & create 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home,
+    redirect: { name: 'Products' },
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/products',
+    name: 'Products',
+    component: () => import('../views/Products.vue'),
+  },
+  {
+    path: '/products/:id',
+    name: 'Product',
+    component: () => import('../views/ProductDetails.vue'),
+  },
+  {
+    path: '*',
+    name: '404',
+    component: () => import('../views/404.vue'),
   },
 ];
 
